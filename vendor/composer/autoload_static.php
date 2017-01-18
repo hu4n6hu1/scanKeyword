fc8e6dea6ee278db86ca937c3272585615bb3e95
l3n641 <371522155@qq.com>
2017-01-18 18:40:18 +0800

    composer phpexcel 库

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -31,6 +31,16 @@ class ComposerStaticInit1749294c202fd1beaefb5d3295a97bfc
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'P' => 
+        array (
+            'PHPExcel' => 
+            array (
+                0 => __DIR__ . '/..' . '/phpoffice/phpexcel/Classes',
+            ),
+        ),
+    );
+
     public static $classMap = array (
         'Callback' => __DIR__ . '/..' . '/jaeger/phpquery-single/phpQuery.php',
         'CallbackBody' => __DIR__ . '/..' . '/jaeger/phpquery-single/phpQuery.php',
@@ -52,6 +62,7 @@ class ComposerStaticInit1749294c202fd1beaefb5d3295a97bfc
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit1749294c202fd1beaefb5d3295a97bfc::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit1749294c202fd1beaefb5d3295a97bfc::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit1749294c202fd1beaefb5d3295a97bfc::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit1749294c202fd1beaefb5d3295a97bfc::$classMap;
 
         }, null, ClassLoader::class);
